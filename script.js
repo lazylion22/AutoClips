90abefef756029eb1488ceb02e2ac64075250f35
Aryep <lazylion2@gmail.com>
2022-01-06 14:24:37 +0200

    Its working

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // listen for messages sent from background.js
     if (request.message === 'hello!') {
-      console.log(request.url) // new url is now in content scripts!
+      console.log('ASDFASDFASDF',request.url) // new url is now in content scripts!
     }
 });
